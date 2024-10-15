4478a3b0c4881de5c8fe3bd928365ffd3ffff134
Justiceman81 <Justiceman814@gmail.com>
2024-10-15 22:20:40 +0300

    start working hw

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,6 +1,13 @@
 import express from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import {
+  createContactSchema,
+  updateContactSchema,
+} from '../validation/contacts.js';
+
+import { validateBody } from '../middlewares/validateBody.js';
+import { isValidId } from '../middlewares/isValidId.js';
+import {
   getAllContactsController,
   getContactByIdController,
   deleteContactController,
@@ -12,13 +19,27 @@ const router = express.Router();
 const jsonParser = express.json({ type: 'application/json' });
 
 router.get('/contacts', ctrlWrapper(getAllContactsController));
-router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
-router.post('/contacts', jsonParser, ctrlWrapper(createContactController));
+router.get(
+  '/contacts/:contactId',
+  isValidId,
+  ctrlWrapper(getContactByIdController),
+);
+router.post(
+  '/contacts',
+  jsonParser,
+  validateBody(createContactSchema),
+  ctrlWrapper(createContactController),
+);
 router.patch(
   '/contacts/:contactId',
   jsonParser,
+  validateBody(updateContactSchema),
   ctrlWrapper(changeContactFavouriteController),
 );
-router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
+router.delete(
+  '/contacts/:contactId',
+  isValidId,
+  ctrlWrapper(deleteContactController),
+);
 
 export default router;
---
+++ src/utils/parseFilterParams.js
@@ -0,0 +1,22 @@
+const parseType = (contactType) => {
+  const isString = typeof contactType === 'string';
+  if (!isString) return;
+  const isKnownType = ['work', 'home', 'personal'].includes(contactType);
+  if (isKnownType(contactType)) return contactType;
+};
+
+const parseFavorite = (isFavourite) => {
+  const isBoolean = typeof isFavourite === 'boolean';
+  if (!isBoolean) return;
+  return isFavourite;
+};
+
+export const parseFilterParams = (query) => {
+  const { contactType, isFavourite } = query;
+  const parsedContactType = parseType(contactType);
+  const parsedIsFavourite = parseFavorite(isFavourite);
+  return {
+    contactType: parsedContactType,
+    isFavourite: parsedIsFavourite,
+  };
+};
