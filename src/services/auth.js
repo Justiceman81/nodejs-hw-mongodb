47b79b3445c94a2f122830ed55e179cdb2036c97
Justiceman81 <Justiceman814@gmail.com>
2024-11-07 23:37:22 +0200

    start working HW

--- src/services/auth.js
+++ src/services/auth.js
@@ -10,6 +10,10 @@ import { sendEmail } from '../utils/sendMail.js';
 import { UsersCollection } from '../db/models/user.js';
 import { SessionsCollection } from '../db/models/session.js';
 import {
+  getFullNameFromGoogleTokenPayload,
+  validateCode,
+} from '../utils/googleOAuth2.js';
+import {
   FIFTEEN_MINUTES,
   THIRTY_DAYS,
   TEMPLATES_DIR,
@@ -175,4 +179,29 @@ export const resetPassword = async (payload) => {
   return {
     status: 204,
   };
+};
+
+export const loginOrSignupWithGoogle = async (code) => {
+  const loginTicket = await validateCode(code);
+  const payload = loginTicket.getPayload();
+  if (!payload) {
+    throw createHttpError(401, 'Unauthorized');
+  }
+
+  let user = await UsersCollection.findOne({ email: payload.email });
+
+  if (!user) {
+    const password = await bcrypt.hash(randomBytes(10), 10);
+    user = await UsersCollection.create({
+      name: getFullNameFromGoogleTokenPayload(payload),
+      email: payload.email,
+      password,
+    });
+  }
+  const newSession = createSession();
+
+  return await SessionsCollection.create({
+    userId: user._id,
+    ...newSession,
+  });
 };
